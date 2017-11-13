dfdb64cb25419dc4c3e227c75c47b806df3a89de
franckaaron <33496107+franckaaron@users.noreply.github.com>
2017-11-13 11:36:55 -0800

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -1,17 +1,65 @@
-package texteditor;
+package textEditor;
+
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class TextEditor {
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-	public static void main(String[] args) {
-	JFrame frame = new JFrame("Text Editor");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	TextPanel tp = new TextPanel();
-	frame.getContentPane().add(tp);
-	frame.pack();
-	frame.setVisible(true);
-	
 
+
+
+public class TextEditor extends JFrame
+{
+	private JTextArea textArea= new JTextArea(20,60);
+	private JFileChooser fc= new JFileChooser();
+	
+		/*
+		 * Constructor for Text Editor Frame
+		 */
+		public TextEditor()
+		{
+			
+		JScrollPane scrollPane= new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		//Adds a filter for text files
+		FileFilter txtFilter= new FileNameExtensionFilter("plain text", "txt");
+		fc.setFileFilter(txtFilter);
+		
+		//Adds a menu bar and menu items
+		add(scrollPane);
+		JMenuBar menuBar= new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu file= new JMenu("File");
+		menuBar.add(file);
+		
+		//Adds items to File menu
+		JMenuItem open= new JMenuItem("Open");
+		JMenuItem save= new JMenuItem("SAve");
+		JMenuItem exit= new JMenuItem("Exit");
+		
+		file.add(open);
+		file.add(save);
+		file.addSeparator();
+		file.add(exit);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+
+		
+		
+		
+		
+		
+		
+		}
+	
+	
+	public static void main (String[] arg) 
+	{
+		new TextEditor();
 	}
 
 }
