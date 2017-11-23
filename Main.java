bc7c8b1602df11bc68521aa6e89da40da344017b
Aaronrls <Aaronrls@yahoo.com>
2017-11-23 00:26:20 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 
-
 import javax.swing.*;
 import java.util.*;
 import java.util.logging.Level;
@@ -8,25 +7,34 @@ import java.util.logging.Logger;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
-import java.awt.Font;
+import java.awt.Color;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.font.TextAttribute;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Document;
+import javax.swing.text.Highlighter;
 
+import javax.swing.text.JTextComponent;
 
 @SuppressWarnings("serial")
-public class Main extends JFrame implements DocumentListener, KeyListener {
+public class Main extends JFrame implements DocumentListener, KeyListener, MouseListener {
 	private Connex con;
 	private WordTable wt;
 	private LinkedList<String> wordss;
+	private LinkedList<String> list;
+	private String content;
+	private Highlighter.HighlightPainter myHighlightPainter;
 	/////////////////////////
 	private JMenu editMenu;
 	private JMenu fileMenu;
@@ -52,12 +60,13 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 	// inserting a suggestion
 	private Mode mode = Mode.INSERT;
 
-	public Main() throws FileNotFoundException, SQLException  {
+	public Main() throws FileNotFoundException, SQLException {
 		super("Project");
 		initComponents();
 
 		textArea.getDocument().addDocumentListener(this);
 		textArea.addKeyListener(this);
+		textArea.addMouseListener(this);
 		InputMap im = textArea.getInputMap();
 		ActionMap am = textArea.getActionMap();
 		// this is where we take control of the enter button. so we can
@@ -65,7 +74,7 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 
 		im.put(KeyStroke.getKeyStroke("ENTER"), COMMIT_ACTION);
 		am.put(COMMIT_ACTION, new CommitAction());
-		
+
 		///// our custom list
 		wordss = new LinkedList<String>();
 		wordss = WordTable.wordss;
@@ -74,6 +83,8 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 
 	private void initComponents() throws FileNotFoundException, SQLException {
 
+		myHighlightPainter = new MyHighlightPainter(Color.pink);
+		list = new LinkedList<String>();
 		//// connection
 		con = new Connex();
 		con.start();
@@ -238,10 +249,10 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 	public void insertUpdate(DocumentEvent ev) {
 		// if the event change happens it returns a 1
 		// this says if it dont return a 1 for some reason dont do nutthin
-		
-		//System.out.println(ev.getLength());
+
+		// System.out.println(ev.getLength());
 		if (ev.getLength() != 1) {
-			
+
 			return;
 		}
 
@@ -277,7 +288,7 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 		}
 		// ok so here is the meat of the program
 		String prefix = content.substring(w + 1).toLowerCase();
-		
+
 		///////////////////////////
 		// System.out.println(prefix);
 		// finds words in list based on matching prefix
@@ -299,125 +310,231 @@ public class Main extends JFrame implements DocumentListener, KeyListener {
 				// calls a completion task which is further down
 				SwingUtilities.invokeLater(new CompletionTask(completion, pos + 1));
 				// System.out.println(content);
-				
-				
-				
+
 			}
 		} else {
 			// Nothing found
-			
-			
+
 			mode = Mode.INSERT;
 
 		}
-	/////	////////////////////// add word shit//////////////////////////////////////////////////
-//		String[] wordsArray = textArea.getText().split("\\s+");
-//				String lastWord = wordsArray[wordsArray.length -1];
-//				
-//				
-//				System.out.println(lastWord);
-		
-		
-		
-		
-////////////////////////////////////////////////////////////////////////////
+		///// ////////////////////// add word
+		///// shit//////////////////////////////////////////////////
+		// String[] wordsArray = textArea.getText().split("\\s+");
+		// String lastWord = wordsArray[wordsArray.length -1];
+		//
+		//
+		// System.out.println(lastWord);
+
+		////////////////////////////////////////////////////////////////////////////
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		//lastWord(e);
-		
+		// lastWord(e);
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
-		lastWord(e);
+
+		try {
+			lastWord(e);
+		} catch (BadLocationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		//lastWord(e);
-		
+		// lastWord(e);
+
 	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void  lastWord(KeyEvent e){
+
+	public void lastWord(KeyEvent e) throws BadLocationException {
 		int key = e.getKeyCode();
-		 if(key == KeyEvent.VK_SPACE){
-		String[] wordsArray = textArea.getText().split("\\s+");
-		String lastWord = wordsArray[wordsArray.length - 1];
-		System.out.println(lastWord);
-		//int n = Collections.binarySearch(wordss, lastWord);
-		
-		if(!wordss.contains(lastWord)){
-						
-			Font font = textArea.getFont();
-			
-			Map attributes = font.getAttributes();
-			attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-			textArea.setFont(font.deriveFont(attributes));
-			wordss.offer(lastWord);
-						
-			wordss.sort(null);
-			System.out.println("inserted");
-			System.out.println(wordss.toString());
-		}
-		
-		 }
-	}
-	
-	
-	
-//	private void checkLastWord() {
-//        try {
-//            int start = Utilities.getWordStart(textArea, textArea.getCaretPosition());
-//            int end = Utilities.getWordEnd(textArea, textArea.getCaretPosition());
-//            String text = textArea.getDocument().getText(start, end - start);
-//           System.out.println(text);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//  }
+		if (key == KeyEvent.VK_SPACE) {
 
+			content = textArea.getText();
+			String words[] = content.split("\\s");
+			for (int i = 0; i < words.length; i++) {
+				list.add(words[i]);
 
-private class CompletionTask implements Runnable {
+			}
+			System.out.println(list.getLast());
 
-	String completion;
-	int position;
-	// takes the suffix suggestion and adds it upon pushing enter
+			for (int j = 0; j < list.size(); j++) {
 
-	CompletionTask(String completion, int position) {
-		this.completion = completion;
-		this.position = position;
+				if (!wordss.contains(list.get(j))) {
 
-	}
+					highlight(textArea, list.get(j));
+				}
+			}
 
-	public void run() {
-		textArea.insert(completion, position);
-		textArea.setCaretPosition(position + completion.length());
-		textArea.moveCaretPosition(position);
-		mode = Mode.COMPLETION;
-	}
-}
-// when the word is completed the carrot needs to be reset with a space at the
-// end of the word
-
-private class CommitAction extends AbstractAction {
-
-	public void actionPerformed(ActionEvent ev) {
-		if (mode == Mode.COMPLETION) {
-			int pos = textArea.getSelectionEnd();
-			textArea.insert(" ", pos);
-			textArea.setCaretPosition(pos + 1);
-			mode = Mode.INSERT;
-		} else {
-			// since the word replace requires us to take control of the return
-			// button , we need to
-			// redefine its original use.
-			textArea.replaceSelection("\n");
 		}
 	}
+
+	public void highlight(JTextComponent comp, String pattern) {
+
+		Highlighter hilite = comp.getHighlighter();
+		Document doc = comp.getDocument();
+
+		try {
+
+			String text = doc.getText(0, doc.getLength());
+			int pos = 0;
+			while ((pos = text.toUpperCase().indexOf(pattern.toUpperCase(), pos)) >= 0) {
+				hilite.addHighlight(pos, pos + pattern.length(), myHighlightPainter);
+				pos += pattern.length();
+			}
+
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	public void dehigh() {
+		textArea.getHighlighter().removeAllHighlights();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// addtext();
+		// wordss.sort(null);
+		// dehigh();
+		//
+
+		int x = e.getX();
+		int y = e.getY();
+
+		System.out.println(x);
+		System.out.println(y);
+		if (isHighlightededUnderMouse(textArea, x, y)) {
+			int jo = JOptionPane.showConfirmDialog(null, "add", "add word to dictionary",
+					JOptionPane.YES_NO_CANCEL_OPTION);
+			if (jo == JOptionPane.YES_OPTION) {
+				addtext();
+				wordss.sort(null);
+				dehigh();
+
+			}
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+		//
+
+		//
+		int textAreaMouseXLocation = e.getXOnScreen();
+		int textAreaMouseYLocation = e.getYOnScreen();
+
+		Point pt = new Point(textAreaMouseXLocation, textAreaMouseYLocation);
+		int pos = textArea.viewToModel(pt);
+		System.out.println(pos);
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// int textAreaMouseXLocation =e.getX();
+		// int textAreaMouseYLocation = e.getY();
+		//
+		// Point pt = new Point(textAreaMouseXLocation, textAreaMouseYLocation);
+		// int pos = textArea.viewToModel(pt);
+		// System.out.println(pos);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public static boolean isHighlightededUnderMouse(JTextComponent textComp, int textAreaMouseXLocation,
+			int textAreaMouseYLocation) {
+		boolean isHighlighted = false;
+		if (textComp.getText().isEmpty()) {
+			return false;
+		}
+
+		Point pt = new Point(textAreaMouseXLocation, textAreaMouseYLocation);
+		int pos = textComp.viewToModel(pt);
+
+		Highlighter.Highlight[] allHighlights = textComp.getHighlighter().getHighlights();
+		String strg = "";
+		for (int i = 0; i < allHighlights.length; i++) {
+			int start = (int) allHighlights[i].getStartOffset();
+			int end = (int) allHighlights[i].getEndOffset();
+			if (pos >= start && pos <= end) {
+				isHighlighted = true;
+				break;
+			}
+		}
+		return isHighlighted;
+	}
+
+	public void addtext() {
+		for (int i = 0; i < list.size(); i++) {
+			wordss.add(list.get(i));
+
+		}
+
+	}
+
+	class MyHighlightPainter extends DefaultHighlighter.DefaultHighlightPainter {
+		public MyHighlightPainter(Color color) {
+			super(color);
+		}
+
+	}
+
+	private class CompletionTask implements Runnable {
+
+		String completion;
+		int position;
+		// takes the suffix suggestion and adds it upon pushing enter
+
+		CompletionTask(String completion, int position) {
+			this.completion = completion;
+			this.position = position;
+
+		}
+
+		public void run() {
+			textArea.insert(completion, position);
+			textArea.setCaretPosition(position + completion.length());
+			textArea.moveCaretPosition(position);
+			mode = Mode.COMPLETION;
+		}
+	}
+	// when the word is completed the carrot needs to be reset with a space at
+	// the
+	// end of the word
+
+	private class CommitAction extends AbstractAction {
+
+		public void actionPerformed(ActionEvent ev) {
+			if (mode == Mode.COMPLETION) {
+				int pos = textArea.getSelectionEnd();
+				textArea.insert(" ", pos);
+				textArea.setCaretPosition(pos + 1);
+				mode = Mode.INSERT;
+			} else {
+				// since the word replace requires us to take control of the
+				// return
+				// button , we need to
+				// redefine its original use.
+				textArea.replaceSelection("\n");
+			}
+		}
 
 	}
 
@@ -436,7 +553,5 @@ private class CommitAction extends AbstractAction {
 			}
 		});
 	}
-
-	
 
 }
